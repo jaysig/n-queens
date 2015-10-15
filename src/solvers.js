c331f0e1bc816c627c40119a15c05df67d59f2bc
Jaysig <jasontsigmon@gmail.com>
2015-10-14 22:18:08 -0400

    Semi successful implementation

--- src/solvers.js
+++ src/solvers.js
@@ -14,10 +14,162 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solutions = []; //fixme
+  //iterate through row 0
+    //with row 0 as a starting point
+    //check at each index how many rooks can be on the rest of the board
+    //when we find a successful configuration
+      //increment count
+      //push solution to array
+  //check hasAnyConflicts based on thi
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  //place a rook somewhere
+  //look through other squares and, if no col or row conflicts, place another rook
+  //count how many rooks we've placed
+
+  //take starting location, place rook there
+  //check through other spaces - where else can we place rooks with no conflicts?
+  //create new board of size n
+  /*
+  [1,0,0]
+  [0,1,0]
+  [0,0,1]
+
+  [1,0,0]
+  [0,0,1]
+  [0,1,0]
+
+  [0,1,0]
+  [1,0,0]
+  [0,0,1]
+
+
+
+  [1,0]
+  [0,0]
+
+  [1,1]
+  [0,0]
+  [1,0]
+  [1,0]
+  [1,0]
+  [0,1]
+
+  [1,1]
+  [0,0]
+
+  [0,0,0]
+
+  recurse([0,0,0]);
+    recurse([1,0,0]);
+    recurse([0,1,0]); [1,0,0]
+    recurse([0,0,1]);[1,0,0]
+
+  var recurse = function (startNode) {
+    if (baseCase) {
+      return;
+    }
+
+    for (var i = 0; i < 3; i++) {
+      startNode[i] = 1;
+      [1,0,0]
+      //recurse(startNode);
+      startNode[i] = 0;
+      [0,0,0]
+    }
+  }
+
+  var recurse = function (startNode) {
+    if (baseCase) {
+      return;
+    }
+
+    for (var i = 0; i < 3; i++) {
+      startNode[i] = 1;
+      //recurse(startNode);
+      startNode[i] = 0;
+    }
+  }
+
+                           11
+                      12        3
+                    14   15         6
+                                    7
+
+  var addTen = function((1 node)) {
+    node.value = node.value + 10;
+    for (var i = 0; i < children.length; i++) {
+      addTen(children[i]);
+    }
+  }
+        var addTen = function((2 node)) {
+            node.value = node.value + 10;
+           for (var i = 0; i < children.length; i++) {
+              addTen(children[i]);
+             }
+            }
+
+                                    var addTen = function((4 node)) {
+                                     node.value = node.value + 10;
+                                    for (var i = 0; i < children.length; i++) {
+                                     addTen(children[i]);
+                                      }
+                                    }
+
+                                    var addTen = function((5 node)) {
+                                     node.value = node.value + 10;
+                                    for (var i = 0; i < children.length; i++) {
+                                     addTen(children[i]);
+                                      }
+                                    }
+
+var arr = [1, 2, 3]
+var solutions = [];
+solutions.push[arr];
+arr[0] = 5;
+[5,2,3]
+console.log(solutions[0]);
+
+
+  [1,1]
+  */
+  var board = new Board({'n': n});
+
+  var piecePlacer = function(rowsToGo, board) {
+    //Node is a board configuration
+    if (rowsToGo === 0) {
+      solutions.push(copyBoard(board));
+      return;
+    }
+
+    for (var row = 0; row < n; row++) {
+      for (var col = 0; col < n; col++) {
+        if (board.get(row)[col]) {
+          continue;
+        }
+        board.togglePiece(row, col);
+        if (!board.hasAnyRooksConflicts()) {
+          piecePlacer(rowsToGo - 1, board);
+        }
+        board.togglePiece(row, col);
+      }
+    }
+  }
+
+  piecePlacer(n, board);
+
+
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solutions[0]));
+  return solutions[0];
+};
+
+var copyBoard = function(board) {
+  var arr = board.rows();
+  var result = [];
+  for (var i = 0; i < arr.length; i++) {
+    result.push(arr[i].slice());
+  }
+  return result;
 };
 
 
